d39180d90353b08cd28dc3cd10b6ff98b5eb44d2
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-08-23 15:00:04 -0300

    Altered repositories

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('appointements')
+@Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
