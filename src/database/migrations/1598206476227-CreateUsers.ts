1886906d558263a97b5b43318c3ec92b97397abc
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-08-23 22:23:29 -0300

    Altered User model

--- src/database/migrations/1598206476227-CreateUsers.ts
+++ src/database/migrations/1598206476227-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1598206476227 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
