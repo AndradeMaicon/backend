f955b4b50913e1f39dd4259021c88f4a2f1bf625
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-11-08 15:25:06 -0300

    Template mail to forgot password created

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -23,7 +23,7 @@ describe('CreateUser', () => {
     expect(user).toHaveProperty('id');
   });
 
-  it('should be able to create a new user with same email from another', async () => {
+  it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
 
