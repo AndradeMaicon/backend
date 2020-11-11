d71d09ba2929659ab00ff5fa006f3fa337e65506
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-11-10 21:01:01 -0300

    List month and days availables of providers

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,62 @@
+import { injectable, inject } from 'tsyringe';
+import { getHours } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+type IResponce = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderDayAvailabiliryService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<IResponce> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        day,
+        month,
+        year,
+      },
+    );
+
+    const hourStart = 8;
+
+    const eachHour = Array.from(
+      { length: 10 },
+      (_, index) => index + hourStart,
+    );
+
+    const availability = eachHour.map(hour => {
+      const hasAppointmentInHour = appointments.find(
+        appointment => getHours(appointment.date) === hour,
+      );
+
+      return {
+        hour,
+        available: !hasAppointmentInHour,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderDayAvailabiliryService;
