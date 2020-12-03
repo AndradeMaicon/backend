babc1a8e66b1e11292d7acbf99ffc16add4a6117
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-12-03 18:58:40 -0300

    Amazon SES

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,20 @@
 interface IMailConfig {
   driver: 'ethereal' | 'ses';
+
+  default: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+  default: {
+    from: {
+      email: 'andrade@maicon.app',
+      name: 'Maicon Andrade',
+    },
+  },
 } as IMailConfig;
